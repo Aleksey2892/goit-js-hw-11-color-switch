f62d1a34b1fd0f781da2c32e9efdde83db238c87
Aleksey <aleksey2892@gmail.com>
2020-07-09 13:49:01 +0300

    ready task

--- src/js/colorSwitch.js
+++ src/js/colorSwitch.js
@@ -1,3 +1,4 @@
+// COLORS DATA
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -7,39 +8,44 @@ const colors = [
   '#795548',
 ];
 
+// DOM REFS
 const refs = {
   Body: document.querySelector('body'),
   startBtn: document.querySelector('button[data-action="start"]'),
   stopBtn: document.querySelector('button[data-action="stop"]'),
 };
 
+// BODY COLOR SWITCH OBJ
+const bodyColor = {
+  intervalId: null,
+  isActive: false,
+
+  startBtn() {
+    if (this.isActive) return;
+
+    this.isActive = true;
+    this.intervalId = setInterval(setColor, 1000);
+    refs.startBtn.setAttribute('disabled', '');
+  },
+
+  stopBtn() {
+    this.isActive = false;
+    clearInterval(this.intervalId);
+    refs.startBtn.removeAttribute('disabled');
+  },
+};
+
+// ADD EVENT LISTENERS
+refs.startBtn.addEventListener('click', bodyColor.startBtn.bind(bodyColor));
+refs.stopBtn.addEventListener('click', bodyColor.stopBtn.bind(bodyColor));
+
+// FUNCTIONS
+const setColor = () => {
+  const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+
+  refs.Body.style.backgroundColor = randomColor;
+};
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-// console.log(randomIntegerFromInterval(0, colors.length - 1));
-
-console.log(refs.startBtn);
-console.log(refs.stopBtn);
-console.log(refs.Body);
-
-// ADD EVENT LISTENERS
-refs.startBtn.addEventListener('click', startBtn);
-refs.stopBtn.addEventListener('click', stopBtn);
-
-// BUTTONS FN
-function startBtn() {
-  console.log('hi');
-}
-
-function stopBtn() {
-  console.log('bye');
-}
-
-function setColors() {
-  let timerId = setInterval(() => {
-    console.log(randomIntegerFromInterval(0, colors.length - 1));
-  }, 1000);
-}
-
-// setColors();
